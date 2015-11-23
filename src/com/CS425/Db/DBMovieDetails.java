982ac055ddb6c0f2ce380cfab4c7f10272ea5ed1
sahil <sahil@104.194.126.166>
2015-11-23 16:18:45 -0600

    16:18 11/23

--- src/com/CS425/Db/DBMovieDetails.java
+++ src/com/CS425/Db/DBMovieDetails.java
@@ -2,14 +2,19 @@ package com.CS425.Db;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class DBMovieDetails {
 
 	static ResultSet rs;
 	static int result;
 	
-	
+	static ArrayList<String> a1= new ArrayList<String>();
+	static ArrayList<String> a2= new ArrayList<String>();
+	static ArrayList<String> a3= new ArrayList<String>();
 
 	public static void getMovieDetails(String movie){
 		DBConnections.query = "select * from movie where title='"+movie+"'";
@@ -50,10 +55,18 @@ public class DBMovieDetails {
 				System.out.println("Screen Number: "+rs.getString(5));
 				System.out.println("Seats Available: "+rs.getString(6));
 				
-				
+				a1.add(rs.getString(7));
+				a2.add(rs.getString(1));
+				a3.add(rs.getString(2));
 				
 			}
 			
+			
+//		/	System.out.println(ar);
+			/*System.out.println(a1);
+			System.out.println(a2);
+			System.out.println(a3);
+			*/
 			Scanner sc=new Scanner(System.in);
 			System.out.println("Enter the theatre name");
 			
@@ -64,13 +77,28 @@ public class DBMovieDetails {
 			System.out.println("Enter the quantity");
 			int quantity=Integer.parseInt(sc.nextLine());
 			
-			if(name.equals(rs.getString(7)) && time.equals(rs.getString(1)) && quantity<=Integer.parseInt(rs.getString(2)))
+			/*if(name.equals(rs.getString(7)) && time.equals(rs.getString(1)) && quantity<=Integer.parseInt(rs.getString(2)))
 			{
 				
 					System.out.println("Success");
 				
 				
+			}*/
+			
+			System.out.println();
+			
+			java.util.Iterator<String> itr=a1.iterator();
+			
+			while(itr.hasNext())
+			{
+				if(name.equals(itr.next()) && a2.contains(time))
+				{
+					System.out.println("The order details are as follows");
+					DBOrderDetails.OrderDeatils();
+				}
 			}
+		
+			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
---
+++ src/com/CS425/Db/DBOrderDetails.java
@@ -0,0 +1,17 @@
+package com.CS425.Db;
+
+import java.sql.ResultSet;
+
+public class DBOrderDetails {
+	
+	
+	static ResultSet rs;
+	static int result;
+	
+	public static void OrderDeatils()
+	{
+		//DBConnections.query=
+	}
+	
+	
+}
