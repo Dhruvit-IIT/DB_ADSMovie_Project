62ab58c31b2c1edf82ae4a1f2cdf0c0f5e47d837
Dhruvit-IIT <dhruvit@104.194.112.182>
2015-11-29 17:42:38 -0600

    11/29 05:42

--- src/com/CS425/Logic/ViewRegisteredUserDetails.java
+++ src/com/CS425/Logic/ViewRegisteredUserDetails.java
@@ -6,16 +6,18 @@ import com.CS425.Db.DBQueries;
 
 public class ViewRegisteredUserDetails {
 
-	
+
 	public void viewRegisteredUserDetails()
 	{
 		DBQueries.viewRegisteredUserDetails();
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter member ID to see more details or R to return");
-		String member_id = input.nextLine();
-		if(member_id.toUpperCase().equals("R"))
-			return;
-		else
-			DBQueries.viewDetailedRegisteredUserDetails(member_id);
+		while(true){
+			System.out.println("Enter member ID to see more details or R to return");
+			String member_id = input.nextLine();
+			if(member_id.toUpperCase().equals("R"))
+				return;
+			else
+				DBQueries.viewDetailedRegisteredUserDetails(member_id);
+		}
 	}
 }
